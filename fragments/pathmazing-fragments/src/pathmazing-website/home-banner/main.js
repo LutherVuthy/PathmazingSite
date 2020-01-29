4c3015784eb52036f0f7d59af3fba7f2a85075f5
LutherVuthy <vathnadev@gmail.com>
2020-01-29 10:19:52 +0700

    Added Custom Colors

--- fragments/pathmazing-fragments/src/pathmazing-website/home-banner/main.js
+++ fragments/pathmazing-fragments/src/pathmazing-website/home-banner/main.js
@@ -1,1 +1,19 @@
 const content = fragmentElement.querySelector('.home-banner');
+
+let images = [
+  'https://pathmazing.com/img/home-banner.png',
+  'https://pathmazing.com/img/bg-home1-new.jpg',
+  'https://pathmazing.com/img/bg-home2-new.jpg',
+  'https://pathmazing.com/img/bg-home3-new.jpg',
+];
+// let index = 0;
+// setInterval(function () {
+//   $("#banner-image")
+//     .fadeOut(function () {
+//       $('#banner-image').css("background-image", "url(" + images[index] + ")");
+//     })
+//     .fadeIn('slow');
+//
+//   index++;
+//   if (index === images.length) index = 0;
+// }, 3000);
