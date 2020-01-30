a8b9c0a6703b80be66106e62bbbe92b618f64227
Ry-Sarim <sarimrysarim@gmail.com>
2020-01-30 12:11:23 +0700

    Update: - Home Banner - Home About Company

--- fragments/pathmazing-fragments/src/pathmazing-website/home-banner/main.js
+++ fragments/pathmazing-fragments/src/pathmazing-website/home-banner/main.js
@@ -1,10 +1,10 @@
-const content = fragmentElement.querySelector('.home-banner');
+const content = fragmentElement.querySelector(".home-banner");
 
 let images = [
-  'https://pathmazing.com/img/home-banner.png',
-  'https://pathmazing.com/img/bg-home1-new.jpg',
-  'https://pathmazing.com/img/bg-home2-new.jpg',
-  'https://pathmazing.com/img/bg-home3-new.jpg',
+  "https://pathmazing.com/img/home-banner.png",
+  "https://pathmazing.com/img/bg-home1-new.jpg",
+  "https://pathmazing.com/img/bg-home2-new.jpg",
+  "https://pathmazing.com/img/bg-home3-new.jpg"
 ];
 // let index = 0;
 // setInterval(function () {
@@ -17,3 +17,14 @@ let images = [
 //   index++;
 //   if (index === images.length) index = 0;
 // }, 3000);
+
+// slider homepage
+$("#slideshow > div:gt(0)").hide();
+setInterval(function() {
+  $("#slideshow > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo("#slideshow");
+}, 4000);
